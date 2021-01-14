ac0f7023a03847821ed0530854e81598468ea35a
whosaidhuh <huh@whosaidhuh.me>
2021-01-14 17:50:16 -0500

    basic tab navigation from expo and a main home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        )
+    }
+}
+
+export default HomeScreen
